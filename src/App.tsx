1cb9c50fec1f158a5a4932956f79c5b93f43e8ba
tavaruaisla-sys <tavaruaisla@gmail.com>
2026-03-11 15:13:26 +0700

    fix: Vercel deployment errors (imports and paths)

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { DefaultProviders } from "./components/providers/default.tsx";
-import AuthCallback from "./pages/auth/Callback.tsx";
-import Index from "./pages/Index.tsx";
-import NotFound from "./pages/NotFound.tsx";
+import { DefaultProviders } from "./components/providers/default";
+import AuthCallback from "./pages/auth/Callback";
+import Index from "./pages/Index";
+import NotFound from "./pages/NotFound";
 
 export default function App() {
   return (
